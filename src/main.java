9b365b56ad059ce8cb5d48d1a66ee0d780fd3b25
Presotto <gabrielpresotto268@gmail.com>
2023-01-23 16:57:12 -0300

    Mudancas no sistema inicial, sistema de randomizacao e nos menus

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
-import escolhedor.escolhedor;
 import menu.menu;
 
 public class main {
@@ -12,61 +8,7 @@ public class main {
 		
 		Scanner teclado = new Scanner(System.in);
 		
-		boolean continua = true;
-		do {
-			
-			int opcao1 = menu.principal(teclado);
-			
-		switch(opcao1) {
-		
-		case 1:
-			
-			menu.escolhaDaCategoria();
-			int escolhaC = teclado.nextInt();
-			switch(escolhaC) {
-				case 1:
-					escolhedor.perguntasFamosos(teclado, 0);
-					break;
-				case 2:
-					escolhedor.perguntasComidas();
-					break;
-				case 3: 
-					escolhedor.perguntasConhecimentoG();
-					
-				case 4:
-					break;
-				
-				case 0:
-					continua = false;
-					break;
-					
-				default:
-					System.out.println("Opção inválida, tente novamente.");
-					menu.escolhaDaCategoria();
-			}
-			
-			break;
-		
-		case 2:
-			menu.creditos();
-			int sair = teclado.nextInt();
-			if(sair == 0) {
-				break;
-			}
-			
-			break;
-			
-		case 0:
-			
-			continua = false;
-			break;
-			
-		default:
-			System.out.println("Opção Inválida");
-			break;
-		
-		}	
-		}while(continua);
+		menu.principal(teclado);
 		
 		teclado.close();
 	}
